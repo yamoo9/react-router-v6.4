557c6b254e2ec1b29315ce10a9e10abd28031148
yamoo9 <yamoo9@naver.com>
2023-01-17 12:21:03 +0900

    complete. 라우터 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import '@/styles/global.css';
 import App from '@/app/App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const container = document.getElementById('root') as HTMLElement;
 
 createRoot(container).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
