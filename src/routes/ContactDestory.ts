16ab0639874bf48b33f6119f9f9895d8b98a9550
yamoo9 <yamoo9@naver.com>
2023-01-18 08:35:03 +0900

    repair. 의도적 "문맥상 오류 처리" 주석

--- src/routes/ContactDestory.ts
+++ src/routes/ContactDestory.ts
@@ -2,7 +2,7 @@ import { redirect } from 'react-router-dom';
 import { deleteContact } from '@/services/contacts';
 
 export async function action({ params }: any) {
-  throw new Error('oh dang!');
+  // throw new Error('oh dang!');
   await deleteContact(params.contactId);
   return redirect('/');
 }
