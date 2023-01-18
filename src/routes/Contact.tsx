d50d31625045429f63c8f7a400522d1eaba390b5
yamoo9 <yamoo9@naver.com>
2023-01-18 12:47:58 +0900

    complete. 데이터 Not found

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -63,7 +63,14 @@ export default function Contact() {
 }
 
 export async function loader({ params }: LoaderParams) {
-  return getContact(params.contactId);
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response('', {
+      status: 404,
+      statusText: 'Not Found',
+    });
+  }
+  return contact;
 }
 
 export async function action({ request, params }: any) {
